1e330d640d6b6ea3c1c60dce97255aa0864b16cf
lay-by <44959695+lay-by@users.noreply.github.com>
2022-01-02 16:42:23 -0600

    added script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+GREEN='\033[0;32m'
+LATEST=$(curl --silent https://api.github.com/repos/R2Northstar/Northstar/releases/latest | jq '.name')
+if [ ! -f "version" ];then
+    echo "0.0.0" > version
+fi
+CURVERS=$(cat version)
+if [ $LATEST != $CURVERS ]; then
+    TEMP=$(curl --silent https://api.github.com/repos/R2Northstar/Northstar/releases/latest | jq -r '.assets'[0].browser_download_url)
+    wget ${TEMP}
+    unzip -o *.zip
+    mv "NorthstarLauncher.exe" "Titanfall2.exe"
+    rm *.zip
+    echo "\n${GREEN}Installed version $LATEST\n"
+    echo $LATEST > version
+else
+    echo "\n${GREEN}You are already on newest version $LATEST\n"
+fi
